46fe7da7eaff94e2c54ada485c82f5491d3f087c
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-08-22 16:49:58 -0300

    Delete Cooler directory

--- Cooler/src/interfaces/IDelegate.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// Function sig taken from gOHM contract
-interface IDelegate {
-    function delegate(address to_) external;
-}
--- Cooler/src/test/mocks/MockStaking.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.0;
-
-contract MockStaking {
-    function unstake(address, uint256 amount, bool, bool) external pure returns (uint256) {
-        return amount;
-    }
-}
